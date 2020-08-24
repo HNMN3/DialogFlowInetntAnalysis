9c27172a661266041f5f88c133397dbc6808e47d
Nitin Kumar Chetwani <hnmn3.nitin@gmail.com>
2020-08-24 18:09:19 +0530

    First draft of anylysis code

---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+
+INPUT_FILE = 'input.csv'
+OUTPUT_FILE = 'output_{}.csv'
+DIALOGFLOW_PROJECT_ID = os.environ.get('PROJECT_ID')
+DIALOGFLOW_LANGUAGE_CODE = 'en-US'
---
+++ main.py
@@ -0,0 +1,77 @@
+import csv
+import sys
+import uuid
+from time import sleep
+
+import dialogflow_v2 as dialogflow
+from google.api_core.exceptions import InvalidArgument
+from tqdm import trange
+
+from constants import (INPUT_FILE, OUTPUT_FILE, DIALOGFLOW_PROJECT_ID,
+                       DIALOGFLOW_LANGUAGE_CODE)
+
+
+def read_file(file_name):
+    try:
+        f_obj = open(file_name)
+        reader = csv.reader(f_obj)
+        # Skip header
+        next(reader)
+        question_phrases = list()
+        for line in reader:
+            input_phrase = line[0]
+            question_phrases.append(input_phrase.strip())
+        return question_phrases
+    except FileNotFoundError:
+        print(f"No file found with name: {file_name}")
+    except Exception as err:
+        print(f"Unable to open the file due to error: {err}")
+    # Quit the program if any file reading is not successful
+    quit()
+
+
+def call_the_api(input_phrase, session_id):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, session_id)
+    text_input = dialogflow.types.TextInput(text=input_phrase,
+                                            language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
+    result = [input_phrase, response.query_result.fulfillment_text,
+              response.query_result.intent.display_name,
+              response.query_result.intent_detection_confidence]
+    return result
+
+
+def write_to_file(output_row, output_file):
+    with open(output_file, 'a') as f_obj:
+        writer = csv.writer(f_obj)
+        writer.writerow(output_row)
+
+
+def main():
+    input_phrases = read_file(INPUT_FILE)
+    input_phrases_len = len(input_phrases)
+    session_id = str(uuid.uuid4())
+    output_file_name = OUTPUT_FILE.format(session_id)
+    header = ["Input Phrase", "Dialogflow Response", "Matched Intent(s)", "Confidence"]
+    write_to_file(header, output_file_name)
+
+    for i in trange(input_phrases_len, file=sys.stdout, desc='DialogFlow API Calls'):
+        input_phrase = input_phrases[i]
+        try:
+            output = call_the_api(input_phrase, session_id)
+        except Exception as err:
+            output = [input_phrase, f"Unable to get output due to err: {err}"]
+
+        write_to_file(output, output_file_name)
+        sleep(1)
+
+    print(f"Output stored to file: {output_file_name}")
+
+
+if __name__ == '__main__':
+    main()
